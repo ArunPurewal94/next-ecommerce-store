c66d13cb534e4021e6707096e89c1faf24df3e19
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-10-23 20:35:13 +0300

    fix: add Link

--- app/product/components/product-card.tsx
+++ app/product/components/product-card.tsx
@@ -4,6 +4,7 @@ import { FormatPrice } from "@/lib/utils";
 import { TruncateText } from "@/lib/utils";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface ProductCardProps {
   data: any;
@@ -13,8 +14,8 @@ export const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   const router = useRouter();
 
   return (
-    <div
-      onClick={() => router.push(`/product/${data.id}`)}
+    <Link
+      href={`/product/${data.id}`}
       className="col-span-1 cursor-pointer border border-slate-200 bg-slate-50 rounded p-2 transition hover:scale-105 text-center text-sm"
     >
       <div className="flex flex-col items-center w-full gap-1">
@@ -31,6 +32,6 @@ export const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="mt-4 uppercase">{TruncateText(data.name)}</div>
         <div>{FormatPrice(data.price)}</div>
       </div>
-    </div>
+    </Link>
   );
 };
