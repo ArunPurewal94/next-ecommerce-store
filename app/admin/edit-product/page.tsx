9227823969784bcf0c51a0cf5df3f3955609e5e3
Arun <arunpurewal94@gmail.com>
2023-10-24 15:32:29 +0100

    todo: implement edit-product logic

--- app/admin/edit-product/page.tsx
+++ app/admin/edit-product/page.tsx
@@ -3,9 +3,11 @@ import { FormWrap } from "@/components/ui/form-wrap";
 import { getCurrentUser } from "@/actions/get-current-user";
 import { AccessDenied } from "@/components/access-denied";
 import { EditProductForm } from "./components/edit-product-form";
+import getProductById from "@/actions/get-product-by-id";
 
 export default async function AddProductsPage() {
   const currentUser = await getCurrentUser();
+  const products = await getProductById({});
 
   if (!currentUser || currentUser.role !== "ADMIN") {
     return <AccessDenied title="Oops! Access Denied" />;
